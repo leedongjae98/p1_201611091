cf43a190ed1bc161d5a0df985b28225607001d64
leedongjad98 <toradong0131@naver.com>
2016-04-11 14:25:28 +0900

    homework

---
+++ sumList (2).py
@@ -0,0 +1,13 @@
+﻿x=list()
+def sumList(aList):
+
+	sum=0
+	for i in range(0,aList):
+		if(i%4==0 and i%5):
+			x.append(i)
+	for i in range(0,len(x)):
+		sum+=x[i]
+	return sum
+def main(x):
+	print sumList(x)
+main(1001)
