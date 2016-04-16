a96f1178a9522925057233f2228209e0bf7555d4
leedongjad98 <toradong0131@naver.com>
2016-04-16 14:04:09 +0900

    updown

---
+++ updown.py
@@ -0,0 +1,17 @@
+﻿def updown(m):
+	import random
+	n=random.randint(1,m)
+	for i in range(1,11):
+		sel=raw_input("Guess number:")
+		sel=int(sel)
+		if sel==n:
+			print "Good! you win!"
+			break
+		elif sel>n:
+			print "Down!"
+		elif sel<n:
+			print "Up!"
+	print n
+	raw_input("Press any botton:")
+
+updown(101)
