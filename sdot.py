a7697074ee5b54cb7f88f2d9c0ff9dbc9086356e
leedongjad98 <toradong0131@naver.com>
2016-04-18 13:51:13 +0900

    w7

---
+++ sdot.py
@@ -0,0 +1,20 @@
+﻿def drawSquareAt(size,pos):
+	dot=list()
+	t1.penup()
+	t1.setpos(pos)
+	t1.pendown()
+	for i in range(0,4):
+		dot.append(i)
+	for i in range(0,4):
+		dot[i]=t1.pos()
+		t1.fd(size)
+		t1.right(90)
+	print dot
+
+import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+pos=(0,0)
+
+drawSquareAt(100,pos)
