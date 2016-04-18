58bd7860d954e9fb8160bd2bbfc5804183a39821
leedongjad98 <toradong0131@naver.com>
2016-04-18 14:24:34 +0900

    w7_02

---
+++ sdot2.py
@@ -0,0 +1,27 @@
+﻿
+dott=list()
+for i in range(0,4):
+	dott.append(i)
+
+dott[0]=(0.00,0.00)
+dott[1]=(100.00,0.00)
+dott[2]=(100.00,-100.00)
+dott[3]=(0.00,-100.00)
+
+def drawSquareFrom(size,dott):
+	for i in range(0,4):
+		t1.penup()
+		t1.setpos(dott[i])
+		t1.pendown()
+		t1.fd(size)
+		t1.right(90)
+
+
+import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+pos=(0,0)
+	
+
+drawSquareFrom(100,dott)
