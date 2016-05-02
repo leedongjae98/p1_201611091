9942cafd6faf24875bff37228809647844529677
leedongjae98 <toradong0131@naver.com>
2016-05-02 13:22:45 +0900

    m9_main()

---
+++ w9_main.py
@@ -0,0 +1,19 @@
+﻿def charcount():
+    word=raw_input('inputword:')
+    d=dict()
+    for c in word:
+        if c not in d:
+            d[c]=1
+        else:
+            d[c]=d[c]+1
+    print d
+
+def lab9():
+	charcount()
+
+def main():
+	lab9()
+
+	
+if __name__=="main":
+	main()
